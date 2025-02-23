3a31c11c6daf9a8bc5a861805f0b3608538261d6
KAI-007CODE <arindamakar17@gmail.com>
2025-02-23 15:26:35 +0530

    done upto cr

--- String_To_Number.py
+++ String_To_Number.py
@@ -1,20 +1,22 @@
 x={"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven":11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen":16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "sventy":70, "eighty":80, "ninety":90, "hundred":100, "thousand":1000, "lakh":100000,"corer":10000000}
 
 str=input("enter a string you want to connnvert to a number: ")
-num=0
+
 def num_to_words(str):
+     str=str.lower()
      str=str.split(" ")
-     print(str)
-     for i in range(len(str)-1):
+     total = 0
+     current = 0
+
+     for  i in range(len(str)):
           if str[i] in x:
-               str[i]=x[str[i]]
-               if str[i]  < 100 :
-                    print (str[i])
-                    # num+=str[i]
+               value=x[str[i]]
+               if value < 100:
+                    current=current+value
                else :
-               #      num = num * str[i]
-                    print(str[i])
-          else :
-               print("the range should not exceed corer")
-     print(num)
+                    current=current*value
+          else:
+               continue #will ignore any misspelled word
+     total = total + current
+     print ("the converted number is ", total)
 num_to_words(str)
