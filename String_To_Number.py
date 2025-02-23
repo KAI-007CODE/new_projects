eb50a349149adaa28cc1eb7b6dad002f2102676f
KAI-007CODE <arindamakar17@gmail.com>
2025-02-23 13:05:22 +0530

    a basic structure

---
+++ String_To_Number.py
@@ -0,0 +1,20 @@
+x={"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven":11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen":16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "sventy":70, "eighty":80, "ninety":90, "hundred":100, "thousand":1000, "lakh":100000,"corer":10000000}
+
+str=input("enter a string you want to connnvert to a number: ")
+num=0
+def num_to_words(str):
+     str=str.split(" ")
+     print(str)
+     for i in range(len(str)-1):
+          if str[i] in x:
+               str[i]=x[str[i]]
+               if str[i]  < 100 :
+                    print (str[i])
+                    # num+=str[i]
+               else :
+               #      num = num * str[i]
+                    print(str[i])
+          else :
+               print("the range should not exceed corer")
+     print(num)
+num_to_words(str)
