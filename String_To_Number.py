117303b6ee59d76dc4775f32c4ea1d52c97e1975
KAI-007CODE <arindamakar17@gmail.com>
2025-03-02 16:43:03 +0530

    made neccesary comments and removed the useless lines

--- String_To_Number.py
+++ String_To_Number.py
@@ -1,25 +1,28 @@
-import re
 count=0
 total=0
-print(a)
+
+#mapped the words of the string to the coresponding number through a dictionary
 x={"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven":11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen":16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty":30, "forty":40, "fifty":50, "sixty":60, "sventy":70, "eighty":80, "ninety":90, "hundred":100, "thousand":1000, "lakh":100000,"corer":10000000}
 
+#took a string as a input
 str=input("enter a string you want to connnvert to a number: ")
 a=str.split( )
-y=["hundred", "thousand", "lakh", "corer"]
 
+#entered the wordes that are to be removed from the list
+y=["hundred", "thousand", "lakh", "corer"]
 for i in  y:
      if i in a :
           a.remove(i)
 
+#gave the words their respective number
 for i in range (0,len(a)):
      a[i]=x[a[i]]
 
 print(a)
 a.reverse()
 print(a)
-#100000000
 
+#the main loop which calculates the value
 for i in range(0,len(a)):
      if i<2:
           count=count+a[i]
@@ -38,9 +41,5 @@ for i in range(0,len(a)):
           print(count)
      else:
           continue
-
-
 total=count
-     
 print("the given number in the string is equal to ", total)
-
